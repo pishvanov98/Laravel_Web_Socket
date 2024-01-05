74202730ead4c1eaa4000f962721edaade43520e
Nikita <pishvanov98@gmail.com>
2024-01-06 01:08:30 +0300

    Подключил веб сокет, отправку сообщения и обновление

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\StoreMessageEvent;
 use Carbon\Carbon;
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
     public function index(){
 
         $message=Message::latest()->get();
-
-        return view('message.index',['messages'=>$message]);
+        $user_id=Auth::user()->id;
+        return view('message.index',['messages'=>$message,'user'=>$user_id]);
     }
 
     public function store(Request $request){
@@ -24,9 +26,15 @@ class MessageController extends Controller
         $message->body=$data['message'];
         $message->save();
 
+
         $current = Carbon::now();
 
         $message->time=$current->format('Y-m-d H:i:s');
+        $user_id=Auth::user()->id;
+
+        //event(new StoreMessageEvent($message,$message->time)); отправляет всем
+        broadcast(new StoreMessageEvent($message,$message->time,$user_id))->toOthers();// отправка всем кроме текущего пользователя,
+        //у меня не сработало, пришлось отправлять id пользователя что бы определить на фронтенде где не выводить инф.
 
         return response()->json($message);
     }
--- resources/views/message/index.blade.php
+++ resources/views/message/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.appNew')
 @section('content')
 <div>
-    <div class="w-1/2 mx-auto">
+    <div class="w-1/2 mx-auto py-2">
         <div class="mb-4">
             <input id="message" type="text" name="message" class="rounded-full border border-gray-400" placeholder="you message" >
         </div>
@@ -10,6 +10,9 @@
         </div>
     </div>
     @if(isset($messages))
+        <div class="w-1/2 mx-auto text-white py-2">
+            <h3 class="py-2">Message</h3>
+        </div>
         <div id="block_message" class="w-1/2 mx-auto text-white py-6">
             @foreach($messages as $message)
                 <div class="text-sm pb-4 mb-4 border-b border-gray-300">
@@ -22,7 +25,37 @@
     @endif
 </div>
 
+<script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+<script>
 
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher('a07c9f36b2d06535ddf8', {
+        cluster: 'eu'
+    });
+
+    var channel = pusher.subscribe('store_message');
+    channel.bind('store_message', function(data) {
+        var dataWebSocketUser=JSON.stringify(data['user']);
+
+        if(dataWebSocketUser == {{$user}}) {// если это текущий пользователь веб сокет не будет обновлять
+            return;
+        }
+
+            var dataWebSocketId=JSON.stringify(data['message']['id']);
+            var dataWebSocketBody=JSON.stringify(data['message']['body']);
+            dataWebSocketBody = dataWebSocketBody.replace(/^"|"$/g, '');
+            var dataWebSocketTime=JSON.stringify(data['time']);
+            dataWebSocketTime = dataWebSocketTime.replace(/^"|"$/g, '');
+
+            $('#block_message').prepend('<div class="text-sm pb-4 mb-4 border-b border-gray-300"><p>'+dataWebSocketId+'</p><p>'+dataWebSocketBody+'</p><p class="text-right">'+dataWebSocketTime+'</p></div>');
+
+    });
+
+
+
+</script>
     <script>
 
 
@@ -38,8 +71,7 @@
                     message:message,
                 },
                 success:function(data){
-                    alert('success');
-
+                    $('#message').val('');
                     $('#block_message').prepend('<div class="text-sm pb-4 mb-4 border-b border-gray-300"><p>'+data['id']+'</p><p>'+data['body']+'</p><p class="text-right">'+data['time']+'</p></div>');
                 },
             });
